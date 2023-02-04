e809cf21a398cf1a5fa19d747743496c7d6e9516
alexanderjmat <alexanderjmatula@gmail.com>
2023-02-04 15:23:40 -0500

    package.json update 8

--- front-end/src/home/HeaderContent.test.js
+++ front-end/src/home/HeaderContent.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import HeaderContent from './HeaderContent';
 
 describe('HeaderContent component', () => {
