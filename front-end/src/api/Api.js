69ff507c615407f0e2bae8a9ee009b2e0944778b
alexanderjmat <alexanderjmatula@gmail.com>
2023-02-05 13:23:09 -0500

    app update

--- front-end/src/api/Api.js
+++ front-end/src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import cookie from "react-cookies";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://eli5-ai.herokuapp.com/"
 
 class MainAPI {
   //admin methods
