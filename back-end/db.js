26a2de0b75099fd70abebd0bfcf9e8e1dddc343e
alexanderjmat <alexanderjmatula@gmail.com>
2023-02-06 09:38:38 -0500

    update

--- back-end/db.js
+++ back-end/db.js
@@ -7,7 +7,7 @@ let DB_URI
 if (process.env.NODE_ENV == "test") {
   DB_URI = "postgresql:///eli5_ai_test";
 } else {
-  DB_URI = "postgresql:///eli5_ai"
+  DB_URI = process.env.HEROKU_POSTGRESQL_OLIVE_URL;
 }
 
 let db = new Client({
